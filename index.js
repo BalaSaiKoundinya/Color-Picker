7c26cfdaefb715e920ec413cbb9aafbc01ac242d
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 23:58:48 +0530

    Changed background color of button1 dynamically

---
+++ index.js
@@ -0,0 +1,19 @@
+let colorPickerContainer = document.getElementById("colorpickercontainer");
+
+
+function firstButton () {
+    // console.log("Button1 is clicked successfully!");
+    colorPickerContainer.style.backgroundColor = "#e0e0e0"
+}
+
+function secondButton () {
+    console.log("Button2 is clicked successfully!");
+}
+
+function thirdButton () {
+    console.log("Button3 is clicked successfully!");
+}
+
+function fourthButton () {
+    console.log("Button4 is clicked successfully!");
+}
